bbdc4f9362480f4a1b389ea22e7f34d36fa4976d
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-11-18 12:21:20 +1100

    Provide a travis-specific .npmrc at CI build time

--- travis/build.sh
+++ travis/build.sh
@@ -3,5 +3,6 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     nix-shell --command "mvn --settings travis/maven-settings.xml test"
 else
+    cp scripts/travis/npmrc scripts/.npmrc
     nix-shell --command "mvn --settings travis/maven-settings.xml deploy"
 fi
