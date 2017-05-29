ec154d2ffc641826411fbcbb04a8810162c491e2
lbodor <lazar.bodor@ga.gov.au>
2017-05-29 11:49:00 +1000

    In CI, run `mvn package`, not `mvn test`
    Running `mvn package` packages geodesyml-v_0_4-scripts.zip, which is
    required to complete the build.

--- travis/build.sh
+++ travis/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    nix-shell --command "mvn --settings travis/maven-settings.xml test"
+    nix-shell --command "mvn --settings travis/maven-settings.xml package"
 else
     cp scripts/travis/npmrc scripts/.npmrc
     nix-shell --command "mvn --settings travis/maven-settings.xml deploy"
